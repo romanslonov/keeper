0a52d40604468be7753006ce9bf5b68a2715b875
Roman <destabiliseus@gmail.com>
2019-06-10 11:38:13 +0800

    Init frontend

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    App status: {{ status }}
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'home',
+  data() {
+    return {
+      status: '',
+    };
+  },
+  components: {
+    HelloWorld,
+  },
+  created() {
+    this.checkHealth();
+  },
+  methods: {
+    checkHealth() {
+      return fetch('https://localhost:3000/api/v1/health/')
+        .then(response => response.json())
+        .then(({ status }) => {
+          this.status = status;
+        })
+        .catch(err => console.error(err));
+    },
+  },
+};
+</script>
