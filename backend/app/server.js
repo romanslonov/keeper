d8c31dcbc3b5c9165d179cd40024a9c017b64d12
Roman <destabiliseus@gmail.com>
2019-06-11 23:19:47 +0800

    Use dotenv, handle 404 rest api reqs

--- backend/app/server.js
+++ backend/app/server.js
@@ -1,12 +1,17 @@
+const dotenv = require('dotenv');
 const express = require('express');
 const https = require('https');
+const http = require('http');
 const app = express();
 const fs = require('fs');
-const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const logger = require('./utils/logger');
+const helmet = require('helmet');
 
+dotenv.config();
+
+app.use(helmet());
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,11 +20,9 @@ fs.readdirSync(`${__dirname}/routes/v1`).forEach((file) => {
     require(`./routes/v1/${file}`)(app);
 });
 
-// Serve vue static files
-app.use(express.static(path.resolve(__dirname, '../../frontend/dist/')));
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../../frontend/dist/index.html'));
-});
+app.get('*', (req, res) => res.status(404).json({
+    message: "Seems like the endpoint you're looking for no longer exists 🤔",
+}));
 
 app.use((err, req, res, next) => {
     if (err) {
@@ -30,11 +33,20 @@ app.use((err, req, res, next) => {
     next();
 });
 
-https
+
+if (process.env.NODE_ENV === 'production') {
+    http
+        .createServer(app)
+        .listen(process.env.PORT, process.env.HOSTNAME, () => {
+            console.log(`Server running at http://${process.env.HOSTNAME}:${process.env.PORT}`);
+        });
+} else {
+    https
     .createServer({
         key: fs.readFileSync('app/security/key.pem'), 
         cert: fs.readFileSync('app/security/cert.pem'),
     }, app)
-    .listen(3000, () => {
+    .listen(process.env.PORT, () => {
         console.log('Server running at https://localhost:3000');
     });
+}
