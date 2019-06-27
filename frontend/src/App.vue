5c03fedce56295468fa9744a367761b96bc76e06
Roman <destabiliseus@gmail.com>
2019-06-28 01:38:12 +0800

    Refactored layouts

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
@@ -25,7 +21,7 @@
   color: #2c3e50;
 }
 
-#nav a.router-link-exact-active {
+#nav a.is-active {
   color: #42b983;
 }
 </style>
---
+++ frontend/src/layouts/Main.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="main-layout">
+        <div id="nav">
+            <router-link exact to="/">Upload</router-link> |
+            <router-link to="/about">Images</router-link>
+        </div>
+        <router-view/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MainLayout'
+}
+</script>
