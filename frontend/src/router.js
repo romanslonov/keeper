9e2da7df88e69225c41d478420d87eb912df8acb
Roman <destabiliseus@gmail.com>
2019-06-28 01:38:31 +0800

    Refactored router

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,25 +1,64 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import MainLayout from './layouts/Main.vue';
+import AuthLayout from './layouts/Auth.vue';
+import store from './store';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
+  linkActiveClass: 'is-active',
+  linkExactActiveClass: 'is-exact-active',
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home,
+      component: MainLayout,
+      meta: { requireAuth: true },
+      children: [
+        {
+          path: '',
+          name: 'home',
+          component: () => import(/* webpackChunkName: "home" */ './views/Home.vue'),
+        },
+        {
+          path: 'about',
+          name: 'about',
+          component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+        },
+      ]
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      path: '/auth',
+      component: AuthLayout,
+      children: [
+        { path: '', redirect: { name: 'Login' }},
+        {
+          path: 'login',
+          name: 'Login',
+          component: () => import(/* webpackChunkName: "login" */ './views/Login.vue'),
+        },
+        {
+          path: 'register',
+          name: 'Register',
+          component: () => import(/* webpackChunkName: "register" */ './views/Register.vue'),
+        },
+      ],
     },
   ],
 });
+
+router.beforeEach(async (to, from, next) => {
+  if (to.matched.some(record => record.meta.requireAuth)) {
+    if (!store.getters.isAuthenticated) {
+      return next({ name: 'Login' });
+    }
+
+    await store.dispatch('getProfile');
+  }
+
+  return next();
+});
+
+export default router;
