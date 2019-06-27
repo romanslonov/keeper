6b05e79306e2886a130f1b9869023e1f51ae2d99
Roman <destabiliseus@gmail.com>
2019-06-28 01:38:43 +0800

    Refactored fetch

--- frontend/src/fetch.js
+++ frontend/src/fetch.js
@@ -1,12 +1,20 @@
+import Auth from './Auth';
+import router from './router';
 import { API_ROOT } from './constants';
 
 export default function request(_path, _options) {
   const options = Object.assign({
+    hasToken: true,
     headers: {
       Accept: 'application/json',
+      Authorization: `Bearer ${Auth.getToken()}`,
       'Content-Type': 'application/json',
     },
   }, _options);
+
+  if (!options.hasToken) {
+    delete options.headers.Authorization;
+  }
 
   if (options.body instanceof FormData) {
     delete options.headers['Content-Type'];
@@ -19,6 +27,10 @@ export default function request(_path, _options) {
       const { status } = response;
 
       if (status >= 400) {
+        if (status === 401) {
+          Auth.deauthenticateUser();
+          router.push({ name: 'Login' });
+        }
         reject(response);
       } else {
         accept(response);
