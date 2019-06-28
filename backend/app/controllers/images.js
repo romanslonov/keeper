0610e516fd6ed58dc2ffb0cafa45d58c673eed1e
Roman <destabiliseus@gmail.com>
2019-06-29 01:40:23 +0800

    Added remove route, refactored images controller

--- backend/app/controllers/images.js
+++ backend/app/controllers/images.js
@@ -1,11 +1,11 @@
 const pool = require('../db');
 
-module.exports = async (req, res) => {
+const getAll = async (req, res) => {
     let connection;
     try {
         connection = await pool.getConnection();
 
-        const images = await connection.query('SELECT * FROM images');
+        const images = await connection.query('SELECT * FROM images WHERE deletedAt IS NULL');
 
         res.status(200).json({ images: images[0] });
     } catch(error) {
@@ -14,4 +14,30 @@ module.exports = async (req, res) => {
     } finally {
         if (connection) return connection.release();
     }
+};
+
+const remove = async (req, res) => {
+    const ids = req.body;
+
+    let connection;
+    try {
+        connection = await pool.getConnection();
+        const now = new Date().toISOString().slice(0, 19).replace('T', ' ');
+        await connection.query(
+            'UPDATE images SET deletedAt = (?) WHERE id IN (?)', 
+            [now, [ids]]
+        );
+
+        res.status(201).json({});
+    } catch(error) {
+        console.log(error);
+        return res.status(500).json({ message: 'Internal server error' });
+    } finally {
+        if (connection) return connection.release();
+    }
+};
+
+module.exports = {
+    getAll,
+    remove,
 };
