06e1bd557da1e41f8df441e0ffe531a5210f67a7
Roman <destabiliseus@gmail.com>
2019-07-09 13:51:38 +0800

    Refactored upload

--- backend/app/controllers/upload.js
+++ backend/app/controllers/upload.js
@@ -9,22 +9,22 @@ module.exports = async (req, res) => {
 
         const queries = files.map((file) => {
             return connection.query(
-                'INSERT INTO images SET ?', 
+                'INSERT INTO images SET ?',
                 {
                     userId: user.id,
-                    fileName: file.filename, 
-                    originalName: file.originalname, 
-                    size: file.size, 
-                    mimeType: file.mimetype, 
-                    destination: file.destination, 
-                    path: file.path, 
+                    fileName: file.filename,
+                    originalName: file.originalname,
+                    size: file.size,
+                    mimeType: file.mimetype,
+                    destination: file.destination,
+                    path: file.path,
                 }
             );
         });
 
         let response = await Promise.all(queries);
 
-        response = response.map(res => ({ ...res[0]}));
+        response = response.map(res => ({ ...res[0] }));
 
         const requested = response.map(res => (`${res.insertId}`));
 
@@ -32,9 +32,9 @@ module.exports = async (req, res) => {
             'SELECT * FROM images WHERE id IN (?)',
             [requested],
         );
-        
+
         res.status(200).json({ images: images[0] });
-    } catch(error) {
+    } catch (error) {
         console.log(error);
         return res.status(500).json({ message: 'Internal server error' });
     } finally {
