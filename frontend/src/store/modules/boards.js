9df9cdfa44b918b3805caa6b6032ce0e7c5570be
Roman <destabiliseus@gmail.com>
2019-07-10 00:55:23 +0800

    Added mutations for boards store module

--- frontend/src/store/modules/boards.js
+++ frontend/src/store/modules/boards.js
@@ -10,27 +10,34 @@ export default {
         fetched: (state) => state.fetched,
         boards: (state) => state.boards,
     },
+    mutations: {
+        fetch(state, boards) {
+            state.boards = boards;
+            state.fetched = true;
+        },
+        clear(state) {
+            state.boards = [];
+            state.fetched = false;
+        },
+        remove(state, id) {
+            state.boards = state.boards.filter(board => board.id !== id);
+        },
+    },
     actions: {
-        fetch({ state }) {
+        fetch({ commit }) {
             return fetch('/boards/')
                 .then(response => response.json())
-                .then(({ boards }) => {
-                    state.boards = boards;
-                    state.fetched = true;
-                })
+                .then(({ boards }) => commit('fetch', boards))
                 .catch((error) => {
                     throw error;
                 });
         },
-        clear({ state }) {
-            state.boards = [];
-            state.fetched = false;
+        clear({ commit }) {
+            commit('clear')
         },
-        remove({ state }, id) {
+        remove({ commit }, id) {
             return this.$fetch(`/boards/${id}/`, { method: 'DELETE' })
-                .then(() => {
-                    state.boards = state.boards.filter(board => board.id !== id);
-                })
+                .then(() => commit('remove', id))
                 .catch((error) => {
                     throw error;
                 });
