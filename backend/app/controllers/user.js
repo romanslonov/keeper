05fd0b8943c5102d3b3195f3de57386cb7a7e0aa
Roman <destabiliseus@gmail.com>
2019-10-21 16:06:16 +0800

    Added get token route

--- backend/app/controllers/user.js
+++ backend/app/controllers/user.js
@@ -1,7 +1,20 @@
-module.exports = (req, res) => {
+const getProfile = (req, res) => {
   const { user } = req;
 
   delete user.password;
 
   res.status(200).json({ user: req.user });
+};
+
+const getToken = (req, res) => {
+  const { token } = req;
+
+  delete user.password;
+
+  res.status(200).json({ user: req.user });
+};
+
+module.exports = {
+  getProfile,
+  getToken,
 };
--- backend/app/routes/v1/user.js
+++ backend/app/routes/v1/user.js
@@ -2,5 +2,6 @@ const userController = require('../../controllers/user');
 const checkAuthentication = require('../../middleware/checkAuthentication');
 
 module.exports = (app) => {
-    app.get('/api/v1/profile', checkAuthentication, userController);
+    app.get('/api/v1/profile', checkAuthentication, userController.getProfile);
+    app.get('/api/v1/token', checkAuthentication, userController.getToken);
 }
