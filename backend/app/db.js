007fe2f2d9fbc62d744891ce815bc3017346bd76
Roman <destabiliseus@gmail.com>
2019-10-21 16:06:27 +0800

    Specify port for db connection settings

--- backend/app/db.js
+++ backend/app/db.js
@@ -3,6 +3,7 @@ const mysql2 = require('mysql2');
 const pool = mysql2.createPool({
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     connectionLimit: 100,
