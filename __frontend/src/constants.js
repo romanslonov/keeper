209ed888e2d1c3d9d0e3ab5298fc7e814e58e0ad
Roman <destabiliseus@gmail.com>
2019-10-24 16:48:17 +0800

    Removed old frontend folder

--- __frontend/src/constants.js
+++
@@ -1,5 +0,0 @@
-export const ENV = process.env.NODE_ENV;
-export const ENV_PRODUCTION = ENV === 'production';
-export const DOMAIN = ENV_PRODUCTION ? 'http://173.232.234.189': 'http://localhost:3000';
-export const HOST = ENV_PRODUCTION ? `${DOMAIN}:3000` : 'http://localhost:3000';
-export const API_ROOT = `${HOST}/api/v1`;
