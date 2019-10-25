d0d5219afe29e57a6e345381aeb76a0bd9d6117d
Roman <destabiliseus@gmail.com>
2019-10-25 22:49:49 +0800

    Refactored search

--- frontend/components/Search.vue
+++ frontend/components/Search.vue
@@ -1,26 +1,23 @@
 <template>
   <div class="search">
-    <input class="search__input" type="text" placeholder="Search">
+    <v-textbox v-model="query" placeholder="Search..." />
   </div>
 </template>
+
+<script>
+import VTextbox from '~/components/Textbox'
+export default {
+  components: { VTextbox },
+  data () {
+    return {
+      query: ''
+    }
+  }
+}
+</script>
 
 <style>
 .search {
   width: 50%;
-}
-.search__input {
-  height: 40px;
-  background-color: var(--search-bg-color);
-  border: 1px solid var(--border-color);
-  font-family: inherit;
-  padding: 8px;
-  font-size: inherit;
-  border-radius: 4px;
-  width: 100%;
-}
-.search__input:focus {
-  background-color: white;
-  outline: none;
-  border-color: var(--primary);
 }
 </style>
